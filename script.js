a0b806b2161aa33d156cfc1928ed4ff24650fd8e
DENYS_DRIL <udenysbystrov@gmail.com>
2024-10-27 20:10:48 +0100

    6

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// ВАШІ СКРИПТИ
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    document.getElementById('message').innerText = `Регистрация успешна! Ласкаво просимо, ${username}.`;
+
+    // Очищаем форму
+    document.getElementById('registrationForm').reset();
+});
+
+document.getElementById('backToHome').addEventListener('click', function() {
+    window.location.href = 'index.html'; // Переход на главную страницу
+});
